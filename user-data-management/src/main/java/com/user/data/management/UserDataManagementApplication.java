70e0e17ff0950c99c181d38d0cb0d062a1ce44ee
naveenconstrutive <naveenbadigunchala1847992@gmail.com>
2025-07-23 18:49:14 +0530

    Adding new module

---
+++ user-data-management/src/main/java/com/user/data/management/UserDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.user.data.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataManagementApplication.class, args);
+    }
+
+}
---
+++ user-data-management/src/test/java/com/user/data/management/UserDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.data.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
